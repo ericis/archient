2c14c9025b9dcd05c9410dfee7c86762cafd82d9
Eric Swanson <email@iseric.com>
2014-08-20 15:57:34 -0700

    refactor view virtualization into library

--- WebApplication1/Global.asax.cs
+++ WebApplication1/Global.asax.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.Caching;
-using System.Web.Hosting;
-using System.Web.Mvc;
-using System.Web.Routing;
-using Archient.Web.Mvc;
-
-namespace WebApplication1
+﻿namespace WebApplication1
 {
+    using System;
+    using System.Collections;
+    using System.Diagnostics;
+    using System.Web.Caching;
+    using System.Web.Hosting;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+    using Archient.Web.Mvc;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -20,42 +16,17 @@ namespace WebApplication1
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
-            ViewEngines.Engines.Clear();
-            ViewEngines.Engines.Add(new MyVE());
-            //ViewEngines.Engines.Add(new RazorViewEngine());
-
-            HostingEnvironment.RegisterVirtualPathProvider(new MyV());
+            Virtual.virtualizeViews(new MyVFP());
         }
     }
 
-    public class MyVF : VirtualFile
-    {
-        public MyVF(string virtualPath)
-            : base(virtualPath)
-        {
-            Debug.WriteLine("MyVF(" + virtualPath + ")");
-        }
-
-        public override Stream Open()
-        {
-            // #3
-            Debug.WriteLine("MyVF.Open()");
-            var ms = new MemoryStream();
-            var writer = new StreamWriter(ms);
-            writer.Write("@inherits System.Web.Mvc.WebViewPage<dynamic>\n<strong>Virtual @Model.Message!</strong>");
-            writer.Flush();
-            ms.Position = 0;
-            return ms;
-        }
-    }
-
-    public class MyV : VirtualPathProvider
+    public class MyVFP : VirtualPathProvider, IVirtualFileProvider
     {
         public override bool FileExists(string virtualPath)
         {
             // #2
             var exists =
-                string.Compare(virtualPath, "/tada/tada.cshtml", StringComparison.InvariantCultureIgnoreCase) == 0
+                virtualPath.EndsWith("/tada/tada.cshtml", StringComparison.InvariantCultureIgnoreCase)
                 ? true
                 : base.FileExists(virtualPath);
             //exists = exists || virtualPath == "~/v/teedee";
@@ -63,23 +34,9 @@ namespace WebApplication1
             return exists;
         }
 
-        public override bool DirectoryExists(string virtualDir)
+        public override VirtualFile GetFile(string virtualPath)
         {
-            var exists = base.DirectoryExists(virtualDir);
-            Debug.WriteLine("VPP.DirectoryExists(" + virtualDir + ") = " + exists);
-            return exists;
-        }
-
-        public override string CombineVirtualPaths(string basePath, string relativePath)
-        {
-            Debug.WriteLine("VPP.CombineVirtualPaths(" + basePath + ", " + relativePath + ")");
-            return base.CombineVirtualPaths(basePath, relativePath);
-        }
-
-        public override string GetFileHash(string virtualPath, IEnumerable virtualPathDependencies)
-        {
-            Debug.WriteLine("VPP.GetFileHash(" + virtualPath + ")");
-            return base.GetFileHash(virtualPath, virtualPathDependencies);
+            return this.GetVirtualFile(virtualPath).ToWebVirtualFile();
         }
 
         public override CacheDependency GetCacheDependency(string virtualPath, IEnumerable virtualPathDependencies, DateTime utcStart)
@@ -91,73 +48,27 @@ namespace WebApplication1
                 : base.GetCacheDependency(virtualPath, virtualPathDependencies, utcStart);
         }
 
-        public override string GetCacheKey(string virtualPath)
+        public override string GetFileHash(string virtualPath, IEnumerable virtualPathDependencies)
         {
-            Debug.WriteLine("VPP.GetCacheKey(" + virtualPath + ")");
-            return base.GetCacheKey(virtualPath);
+            Debug.WriteLine("VPP.GetFileHash(" + virtualPath + ")");
+            return base.GetFileHash(virtualPath, virtualPathDependencies);
         }
 
-        public override VirtualFile GetFile(string virtualPath)
+        IVirtualFile IVirtualFileProvider.GetFile(string virtualPath)
+        {
+            return this.GetVirtualFile(virtualPath);
+        }
+
+        private IVirtualFile GetVirtualFile(string virtualPath)
         {
             var content = "@inherits System.Web.Mvc.WebViewPage<dynamic>\n<strong>Virtual @Model.Message!</strong>";
 
             Debug.WriteLine("VPP.GetFile(" + virtualPath + ")");
-            
+
             return
                 string.Compare(virtualPath, "/tada/tada.cshtml", StringComparison.InvariantCultureIgnoreCase) == 0
-                ? Virtual.createFileFromString(content, virtualPath).ToWebVirtualFile()
-                : base.GetFile(virtualPath);
-        }
-
-        public override VirtualDirectory GetDirectory(string virtualDir)
-        {
-            Debug.WriteLine("VPP.GetDirectory(" + virtualDir + ")");
-            return base.GetDirectory(virtualDir);
-        }
-    }
-
-    public class MyVE : VirtualPathProviderViewEngine
-    {
-        public MyVE()
-        {
-            this.AreaMasterLocationFormats = new string[0];
-            this.AreaViewLocationFormats = new string[0];
-            this.AreaPartialViewLocationFormats = new string[0];
-
-            foreach (var s in this.MasterLocationFormats ?? new string[0])
-                Debug.WriteLine("MyVE.MasterLocationFormats = " + s);
-            foreach (var s in this.ViewLocationFormats ?? new string[0])
-                Debug.WriteLine("MyVE.ViewLocationFormats = " + s);
-            foreach (var s in this.PartialViewLocationFormats ?? new string[0])
-                Debug.WriteLine("MyVE.PartialViewLocationFormats = " + s);
-
-            this.MasterLocationFormats = new[] { "~/Shared/{1}/{0}.cshtml" };
-            this.ViewLocationFormats = new[] { "~/Views/{1}/{0}.cshtml", "~/Shared/{1}/{0}.cshtml" };
-            this.PartialViewLocationFormats = new[] { "~/Views/{1}/{0}.cshtml", "~/Shared/{1}/{0}.cshtml" };
-        }
-
-        protected override bool FileExists(ControllerContext controllerContext, string virtualPath)
-        {
-            // #1
-            var exists = 
-                string.Compare(virtualPath, "~/tada/tada.cshtml", StringComparison.InvariantCultureIgnoreCase) == 0
-                ? true
-                : base.FileExists(controllerContext, virtualPath);
-            Debug.WriteLine("VPPVE.FileExists(" + virtualPath + ") = " + exists);
-            return exists;
-        }
-
-        protected override IView CreatePartialView(ControllerContext controllerContext, string partialPath)
-        {
-            Debug.WriteLine("VPPVE.CreatePartialView(" + partialPath + ")");
-            return new RazorView(controllerContext, partialPath, null, true, null);
-        }
-
-        protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
-        {
-            var view = new RazorView(controllerContext, viewPath, masterPath, true, null);
-            Debug.WriteLine("VPPVE.CreateView(" + viewPath + ", " + masterPath + ") = " + view.ViewPath);
-            return view;
+                ? Virtual.createFileFromString(content, virtualPath)
+                : base.GetFile(virtualPath).ToVirtualFile();
         }
     }
 }
--- src/Web.Mvc/Virtual.fs
+++ src/Web.Mvc/Virtual.fs
@@ -1,16 +1,25 @@
 ﻿namespace Archient.Web.Mvc
 
+open System
+
 type IVirtualFile =
 
     abstract member VirtualPath : string with get
 
     abstract member Open : unit -> System.IO.Stream
 
+type IVirtualFileProvider =
+    
+    abstract member FileExists : virtualPath:string -> bool
+    abstract member GetFileHash : virtualPath:string * virtualPathDependencies:System.Collections.IEnumerable -> string
+    abstract member GetFile : virtualPath:string -> IVirtualFile
+
 module Virtual =
     
     open System
     open System.IO
     open System.Web.Hosting
+    open System.Web.Mvc
     
     type private MyVirtualFile(virtualPath, onOpen) =
         inherit VirtualFile(virtualPath)
@@ -20,6 +29,45 @@ module Virtual =
         interface IVirtualFile with
             override me.VirtualPath with get() = virtualPath
             override me.Open() = me.Open()
+
+    type private MyVirtualFilePathProvider
+        (
+            onFileExists,
+            onGetFile,
+            onGetCacheDependency) =
+
+        inherit VirtualPathProvider()
+
+        override me.FileExists(virtualPath) =
+            onFileExists virtualPath
+
+        override me.GetCacheDependency(virtualPath, virtualPathDependencies, utcStart) =
+            onGetCacheDependency virtualPath virtualPathDependencies utcStart
+
+        override me.GetFile(virtualPath) =
+            onGetFile virtualPath
+
+    type private VirtualRazorViewEngine(virtualFileProvider:IVirtualFileProvider) as me =
+        inherit VirtualPathProviderViewEngine()
+        
+        do
+            me.AreaMasterLocationFormats <- Array.empty
+            me.AreaViewLocationFormats <- Array.empty
+            me.AreaPartialViewLocationFormats <- Array.empty
+            me.MasterLocationFormats <- Array.empty
+            me.ViewLocationFormats <- [|""|] // empty results in MVC exception
+            me.PartialViewLocationFormats <- Array.empty
+
+        override me.FileExists(controllerContext, virtualPath) =
+            match virtualFileProvider.FileExists(virtualPath) with
+            | true -> true
+            | false -> base.FileExists(controllerContext, virtualPath)
+
+        override me.CreatePartialView(controllerContext, partialPath) = 
+            RazorView(controllerContext, partialPath, null, true, null) :> IView
+
+        override me.CreateView(controllerContext, viewPath, masterPath) =
+            RazorView(controllerContext, viewPath, masterPath, true, null) :> IView
 
     let createFile onOpen virtualPath =
         {
@@ -45,8 +93,50 @@ module Virtual =
     let toWebVirtualFile (virtualFile:IVirtualFile) =
         new MyVirtualFile(virtualFile.VirtualPath, fun _ -> virtualFile.Open()) :> VirtualFile
 
+    let toVirtualFile (virtualFile:VirtualFile) =
+        new MyVirtualFile(virtualFile.VirtualPath, fun _ -> virtualFile.Open()) :> IVirtualFile
+
+    let createVirtualFileProvider onFileExists onGetFileHash onGetFile =
+        {
+            new IVirtualFileProvider with
+                override me.FileExists(virtualPath) = onFileExists virtualPath
+                override me.GetFileHash(virtualPath, virtualPathDependencies) = onGetFileHash virtualPath virtualPathDependencies
+                override me.GetFile(virtualPath) = onGetFile virtualPath
+        }
+
+    let replaceViewEngines virtualEngine =
+        ViewEngines.Engines.Clear()
+        ViewEngines.Engines.Add(virtualEngine)
+
+    let addVirtualPathProvider provider =
+        HostingEnvironment.RegisterVirtualPathProvider(provider)
+
+    let toWebVirtualFileProvider (virtualProvider:IVirtualFileProvider) =
+        let onFileExists path = virtualProvider.FileExists(path)
+        let onGetFile path = virtualProvider.GetFile(path) :?> VirtualFile
+        let onGetCache _ _ _ = null
+        new MyVirtualFilePathProvider(
+            onFileExists, 
+            onGetFile, 
+            onGetCache) :> VirtualPathProvider
+    
+    let createVirtualRazorViewEngine virtualFileProvider =
+        VirtualRazorViewEngine(virtualFileProvider) :> IViewEngine
+
+    let virtualizeViews provider =
+        replaceViewEngines(createVirtualRazorViewEngine provider)
+        addVirtualPathProvider(provider)
+
 [<System.Runtime.CompilerServices.Extension>]
 module VirtualFileExtensions =
     [<System.Runtime.CompilerServices.Extension>]
     let ToWebVirtualFile(virtualFile:IVirtualFile) =
         Virtual.toWebVirtualFile virtualFile
+        
+    [<System.Runtime.CompilerServices.Extension>]
+    let ToVirtualFile(virtualFile:System.Web.Hosting.VirtualFile) =
+        Virtual.toVirtualFile virtualFile
+
+    [<System.Runtime.CompilerServices.Extension>]
+    let ToWebVirtualFileProvider(virtualFileProvider:IVirtualFileProvider) =
+        Virtual.toWebVirtualFileProvider virtualFileProvider
