48ec8178b6769c9a084f7a4c3156723d2673db36
Eric Swanson <email@iseric.com>
2014-08-29 12:57:19 -0700

    Tweets cleanup

---
+++ src/Twitter/Tweets.fs
@@ -0,0 +1,29 @@
+﻿namespace Archient.Twitter
+
+module Tweet =
+    
+    open System.Linq
+    open LinqToTwitter
+
+    let search criteria =
+        
+        let auth = 
+            let key, secret = ("[key]","[secret]")
+            let authorizer = ApplicationOnlyAuthorizer(CredentialStore = InMemoryCredentialStore(ConsumerKey = key, ConsumerSecret = secret))
+            let task = authorizer.AuthorizeAsync()
+            task.Wait()
+            authorizer
+
+        use twitter = new TwitterContext(auth)
+        
+        let query = 
+            twitter.Search.Where(
+                fun search -> 
+                    search.Type = SearchType.Search && 
+                    search.Query = criteria)
+
+        let searchResult = query.FirstOrDefault()
+
+        searchResult.Statuses
+        |> Seq.map (fun tweet -> tweet.Text)
+        |> Seq.toArray
